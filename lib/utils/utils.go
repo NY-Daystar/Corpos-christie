d7361758fe44573e565802866244a99ba1ad0d8f
Lucas <luc4snoga@gmail.com>
2021-07-03 00:41:49 +0200

    Update utils module

--- lib/utils/utils.go
+++ lib/utils/utils.go
@@ -3,7 +3,12 @@ package utils
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
+)
+
+const (
+	DEFAULT_PADDING = 10 // Add defautl padding for function setPadding
 )
 
 // Read input from terminal
@@ -26,4 +31,28 @@ func ConvertStringToInt(str string) (int, error) {
 func GetCurrentYear() int {
 	year, _, _ := time.Now().Date()
 	return year
+}
+
+// Get Max length from slice string
+func GetMaxLength(tab []string) int {
+	var maxIndexLength int
+	for _, v := range tab {
+		if maxIndexLength < len(v) {
+			maxIndexLength = len(v)
+		}
+	}
+	return maxIndexLength
+}
+
+// Get padding between value in tab for each of them to align items
+func getPadding(tab []string) int {
+	return GetMaxLength(tab)
+}
+
+// Set a padding with a value among a list of data
+func SetPadding(tab []string, v string) string {
+	var padding int = getPadding(tab)
+	var gap int = padding - len(v) + DEFAULT_PADDING
+	var space string = strings.Repeat(" ", gap)
+	return space
 }
--- lib/utils/utils_test.go
+++ lib/utils/utils_test.go
@@ -10,12 +10,14 @@ import (
 
 // Test string conversion to int
 func TestStringConvertToInt(t *testing.T) {
-	var vString string = "32000"
+	var stringRef string = "32000"
 
-	_, err := ConvertStringToInt(vString)
+	val, err := ConvertStringToInt(stringRef)
 	// Testing parts
 	if err != nil {
-		t.Errorf("Impossible to convert this string %v, err: %v", vString, err)
+		t.Errorf("Impossible to convert this string %s, err: %v", stringRef, err)
+	} else if val != 32000 {
+		t.Errorf("Value '%d' is not the same as ref '%v'", val, stringRef)
 	}
 }
 
@@ -25,6 +27,62 @@ func TestUserInCoupleNoChildren(t *testing.T) {
 
 	var year = GetCurrentYear()
 	if currentYear != year {
-		t.Errorf("The currentYear '%v' is not return by the function GetCurrentYear() '%v'", currentYear, year)
+		t.Errorf("The currentYear '%d' is not return by the function GetCurrentYear() '%d'", currentYear, year)
+	}
+}
+
+// Test if function return the maxLength among this item
+func TestMaxLength(t *testing.T) {
+	var longItem string = "test max length"
+	var items = []string{"tax", "options", longItem, "db clean"} // 'long item' has to be the long string in array
+	var refMaxLength int = len(longItem)
+	t.Logf("Length of %s : %d", longItem, refMaxLength)
+
+	var maxLength int = GetMaxLength(items)
+	t.Logf("Length find by function: %d", maxLength)
+
+	if refMaxLength != maxLength {
+		t.Errorf("The refMaxLength '%d' is not the same has maxLength '%d'", refMaxLength, maxLength)
+	}
+}
+
+// Test if function return the right padding
+func TestGetPadding(t *testing.T) {
+	var longItem string = "test get padding function"
+	var items = []string{"tax", "options", longItem, "db clean"} // 'long item' has to be the long string in array
+	var refGetPadding int = len(longItem)
+	t.Logf("Length of %s : %d", longItem, refGetPadding)
+
+	var padding int = getPadding(items)
+	t.Logf("Length find by function: %d", padding)
+
+	if refGetPadding != padding {
+		t.Errorf("The refMaxLength '%d' is not the same has maxLength '%d'", refGetPadding, padding)
+	}
+}
+
+// Test if the padding is set
+func TestSetPadding(t *testing.T) {
+	var items = []string{"tax", "options", "db"}
+
+	// get padding for 'tax', 'options', 'db' values
+	var paddings []int
+	for _, val := range items {
+		paddings = append(paddings, len(SetPadding(items, val))+len(val))
+	}
+
+	// check if all value are the same
+	var samePaddings bool = func(a []int) bool {
+		for i := 1; i < len(a); i++ {
+			if a[i] != a[0] {
+				return false
+			}
+		}
+		return true
+	}(paddings)
+
+	// Padding has to be equal at the end between every value if we retranch their length to uniformize it
+	if !samePaddings {
+		t.Errorf("The paddings of paddings in the array %+v are not equal, paddings %+v", items, paddings)
 	}
 }
