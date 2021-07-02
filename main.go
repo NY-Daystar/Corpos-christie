05376f22a234c025bbb3cc89de5092179fd6f11b
Lucas <luc4snoga@gmail.com>
2021-07-03 00:27:53 +0200

    Simplify main program

--- main.go
+++ main.go
@@ -2,65 +2,30 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/LucasNoga/corpos-christie/config"
+	"github.com/LucasNoga/corpos-christie/core"
 	"github.com/LucasNoga/corpos-christie/lib/colors"
-	"github.com/LucasNoga/corpos-christie/tax"
 	"github.com/LucasNoga/corpos-christie/user"
+)
+
+const (
+	APP_NAME    string = "Corpos-Christie"
+	APP_VERSION string = "0.0.8"
 )
 
 // Configuration of the application
 var cfg *config.Config
 
-// Start tax calculator from input user
-func start(cfg *config.Config, user *user.User) bool {
-	// Ask income's user
-	_, err := user.AskIncome()
-	if err != nil {
-		log.Printf("Error: asking income for user, details: %v", err)
-		return false
-	}
-
-	// Ask if user is in couple
-	_, err = user.AskIsInCouple()
-	if err != nil {
-		log.Printf("Error: asking is in couple for user, details: %v", err)
-		return false
-	}
-
-	// Ask if user hasChildren
-	_, err = user.AskHasChildren()
-	if err != nil {
-		log.Printf("Error: asking has children, details: %v", err)
-		return false
-	}
-
-	// Calculate tax
-	result := tax.Process(user, cfg)
-
-	// Show user
-	user.Show()
-
-	// Ask user if he wants to see tax tranches
-	if ok, err := user.AskTaxDetails(); ok {
-		if err != nil {
-			log.Printf("Error: asking tax details, details: %v", err)
-		}
-		tax.ShowTaxTranche(result)
-	}
-
-	return true
-}
-
 // Init configuration file
 func init() {
 	cfg = new(config.Config)
+	cfg.Name = APP_NAME
+	cfg.Version = APP_VERSION
 	_, err := cfg.LoadConfiguration("./config.json")
 	if err != nil {
-		log.Printf(colors.Red("Unable to load config.json file, details: %v"), colors.Red(err))
+		log.Printf(colors.Red("Unable to load config.json file, details: %s"), colors.Red(err))
 		cfg.LoadDefaultConfiguration()
 	}
 
@@ -70,30 +35,8 @@ func init() {
 
 // Starting program
 func main() {
-	log.Printf("Project: %v", colors.Yellow(cfg.Name))
-	log.Printf("Version: %v", colors.Yellow(cfg.Version))
-
 	// Init user
 	var user *user.User = new(user.User)
 
-	// Loop so start program until user wants to exit
-	for {
-		status := start(cfg, user)
-		if status {
-			log.Println(colors.Green("Tax process successful"))
-		} else {
-			log.Println(colors.Red("Tax process failed"))
-		}
-		fmt.Println("--------------------------------------------------------------")
-
-		// ask user to restart program else we exit
-		if user.AskRestart() {
-			log.Printf("Restarting program...")
-			continue
-		}
-		break
-	}
-
-	log.Printf("Program exited...")
-	os.Exit(0)
+	core.Start(cfg, user)
 }
